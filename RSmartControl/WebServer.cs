766ad88d999f73e940f139bc32357c202ee814d8
val92130 <chatelain@intechinfo.fr>
2015-06-08 10:59:55 +0200

    enabling dhcp

--- RSmartControl/WebServer.cs
+++ RSmartControl/WebServer.cs
@@ -30,7 +30,7 @@ namespace RSmartControl
             //Initialize Socket class
             socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             //Request and bind to an IP from DHCP server
-            socket.Bind(new IPEndPoint(ip, 80));
+            socket.Bind(new IPEndPoint(IPAddress.Any, 80));
             //Debug print our IP address
             Debug.Print(Microsoft.SPOT.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces()[0].IPAddress);
             //Start listen for web requests
